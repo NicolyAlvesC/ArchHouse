8d4fadf6056b7641ff43b909f3c3bb0d97cfdfa5
Nicoly Alves <niconicoly9718@gmail.com>
2022-10-09 05:06:48 -0300

    Subindo Projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import './App.css';<
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>apaguei arquivos, teste pra ver se sobe</h1>
       </header>
     </div>
   );
