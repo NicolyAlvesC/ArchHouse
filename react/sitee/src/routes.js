c8f9e62d68610c7429a70dee84b2c620d0a4e808
Nicoly Alves <niconicoly9718@gmail.com>
2022-10-09 12:04:08 -0300

    routes, novamente

--- react/sitee/src/routes.js
+++ react/sitee/src/routes.js
@@ -6,13 +6,10 @@ import Entrada from './pages/entrda/entrada.index';
 export default function Routess() {
     return (
         <BrowserRouter>
-            <main>
                 <Routes>
                     <Route path='/' element={<Entrada/>}/>
                     <Route path='/homepage'  element={<Homepage/>}/>
                 </Routes>
-            </main>
-            
         </BrowserRouter>
     )
 }
